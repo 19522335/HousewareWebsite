a122354850e2c4bc6a59881a680c71217c6abc63
19522335 <19522335@gm.uit.edu.vn>
2022-05-06 11:05:22 +0700

    connect database

--- back-end/src/config/index.js
+++ back-end/src/config/index.js
@@ -19,7 +19,10 @@ module.exports = {connect}*/
 
 const mongoose = require('mongoose')
 //ket noi phan mem voi mongodb qua string duoi
-const connectString = "mongodb+srv://khanh:khanh@cluster0.nhyli.mongodb.net/monastore?retryWrites=true&w=majority"
+const connectString = "mongodb+srv://admin1:admin1@housewarewebsitecluster.pre3w.mongodb.net/cigstore?retryWrites=true&w=majority"
+// mongodb+srv://admin1:admin1@housewarewebsitecluster.pre3w.mongodb.net/cigstore?retryWrites=true&w=majority
+
+
 const connect = async () =>{
     mongoose.connect(connectString, {
         useNewUrlParser: true,
